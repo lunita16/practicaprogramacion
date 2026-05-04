004878d9efb02521ff5b4effb35c2465b60bc074
lunita16 <luluarguello2010@gmail.com>
2026-05-04 09:03:01 -0300

    Add files via upload

---
+++ practica_programacion.py
@@ -0,0 +1,26 @@
+#print("hola mundo")#que hace esta funcion? esta funcion imprime el mensaje y como le di el comando
+
+
+#nombre=input("como te llamas?")
+#print("hola",nombre) #input permite la entrada de datos por teclado
+
+
+
+#palabra=input("escribi un texto")
+#print ("cantidad de letras: ",len(palabra))#esta linea imprime el texto y te devuelve el resultado de cuantas letras estan, contando los espacios
+
+
+#numero=input("ingresa un número:")
+#numero= int(numero)
+#print("el doble es:",numero*2)# imprime el numero y despues a ese resultado hace el doble y a ese numero lo multiplica
+
+#numero=float(input("ingresa un numero decimal:"))
+#print("redondeado:",round(numero))#esto tiene varias funciones en esto, y hace mas corta las lineas y que de el mismo resultado
+#en esta linea de codigo nos nos pide un numero decimal, y entonces despues lo redondea y hace el numeroo mas chico o mas grande dependiendo los numero 
+#invova la funcion argumenta los comandos, y concatenamos lineas 
+
+#crear un programa que:pida un nombr,pida edad en decimal,muestre cuantos caracteres tiene el nombre, y muestre la edad redondeada
+nombre=input("como te llamas?")
+edad=float(input("cual es tu edad decimal?"))
+print("cantidad de letras:",len(nombre))
+numero=print("edad redondeada:",round(numero))
